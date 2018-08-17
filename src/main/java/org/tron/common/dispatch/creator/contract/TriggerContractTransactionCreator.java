2aeecdda764914f8974e6845f8c741e6d93c417a
sasaxie <xiexiaodong917@163.com>
2018-08-17 18:56:47 +0800

    :recycle:trigger contract address can config by program parameter

--- src/main/java/org/tron/common/dispatch/creator/contract/TriggerContractTransactionCreator.java
+++ src/main/java/org/tron/common/dispatch/creator/contract/TriggerContractTransactionCreator.java
@@ -7,8 +7,10 @@ import org.tron.common.dispatch.TransactionFactory;
 import org.tron.common.dispatch.creator.CreatorCounter;
 import org.tron.common.dispatch.creator.transfer.AbstractTransferTransactionCreator;
 import org.tron.common.utils.AbiUtil;
+import org.tron.common.utils.Base58;
 import org.tron.common.utils.ByteArray;
 import org.tron.common.utils.TransactionUtils;
+import org.tron.program.GenerateTransaction;
 import org.tron.protos.Contract;
 import org.tron.protos.Contract.TriggerSmartContract;
 import org.tron.protos.Protocol;
@@ -19,7 +21,8 @@ public class TriggerContractTransactionCreator extends AbstractTransferTransacti
   protected Protocol.Transaction create() {
     TransactionFactory.context.getBean(CreatorCounter.class).put(this.getClass().getName());
 
-    TriggerSmartContract contract = triggerCallContract(ownerAddress.toByteArray(), contractAddress, callValue, org.bouncycastle.util.encoders.Hex
+    TriggerSmartContract contract = triggerCallContract(ownerAddress.toByteArray(), Base58
+        .decodeFromBase58Check(GenerateTransaction.getArgsObj().getContractAddress()), callValue, org.bouncycastle.util.encoders.Hex
         .decode(AbiUtil.parseMethod(methodStr, argsStr, false)));
 
     Protocol.Transaction transaction = TransactionUtils.createTransaction(contract, ContractType.TriggerSmartContract);
